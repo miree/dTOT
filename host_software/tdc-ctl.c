f18a322e126f24f4204686dbfe5b5fb683e4871f
Michael Reese <michaelate@gmail.com>
2019-06-30 14:00:56 +0200

    tdc-ctl: add help message and -h option

--- host_software/tdc-ctl.c
+++ host_software/tdc-ctl.c
@@ -42,6 +42,29 @@ int too_quickly2(double threshold_sec)
 	return result;
 }
 
+void print_help() {
+	printf("usage: tdc-ctl <device> [options]\n");
+	printf("\n");
+	printf("Without options the program will print events from the TDC to stdout.\n");
+	printf("<device> is a serial port where the TDC is connected, e.g. /dev/ttyUSB0\n");
+	printf("\n");
+	printf("available options:\n");
+	printf("-e <enable_pattern>     Enable/disable TDC channels, where <enable_pattern>\n");
+	printf("                        is a 4 character string consisting of '0's and '1's\n ");
+	printf("                        where the character position corresponds to the \n ");
+	printf("                        channel number. Examples: \n");
+	printf("                        '-e0010' enables channel 2 only\n ");
+	printf("                        '-e1001' enables channel 0 and 3\n ");
+	printf("                        '-e0110' enables channel 1 and 2\n ");
+	printf("                        '-e0000' disable all channels\n ");
+	printf("                        '-e1111' enable all channels\n ");
+	printf("-t <channel:threshold>  Set the threshold for a channel, where channel and \n");
+	printf("                        threshold are separated by a colon. Examples:\n ");
+	printf("                        '-t0:0'    set threshold of channel 0 to 0\n ");
+	printf("                        '-t1:4095' set threshold of channel 1 to 4095 (max)\n ");
+	printf("                        '-t2:2000' set threshold of channel 2 to 2000\n ");
+	printf(" -h                     print this help\n");
+}
 
 int main(int argc, char *argv[]) 
 { 
@@ -53,20 +76,29 @@ int main(int argc, char *argv[])
 	int snoop = 1;
 	tdc_t *tdc = 0;
 
+	if (argc == 1) {
+		print_help();
+		return 0;
+	}
+
 	
 	// put ':' in the starting of the 
 	// string so that program can 
 	//distinguish between '?' and ':' 
-	while((opt = getopt(argc, argv, ":se:t:")) != -1) 
+	while((opt = getopt(argc, argv, ":he:t:")) != -1) 
 	{ 
 		switch(opt) 
 		{ 
+			case 'h':
+				print_help();
+				return 0;
+			break;
 			case 'e': 
 				snoop = 0;
 				enable_pattern = 0;
 				for (int i = 0; i < 4; ++i) {
 					if (optarg[i] == '\0' || (optarg[i] != '0' && optarg[i] != '1')) {
-						fprintf(stderr, "invalid enable bit pattern %s, must be a sequenc of for characters: '0' or '1'\n", optarg);
+						fprintf(stderr, "invalid enable bit pattern %s, must be a sequence of for characters: '0' or '1'\n", optarg);
 					} else {
 						if (optarg[i] == '1') {
 							enable_pattern |= 1<<i;
@@ -84,19 +116,23 @@ int main(int argc, char *argv[])
 				sscanf(optarg,"%d:%d",&channel,&threshold);
 				if (threshold < 0 || threshold >= TDC_THRESHOLD_RANGE) {
 					fprintf(stderr, "invalid threshold %d, must be in range [%d,%d]\n", threshold, 0, TDC_THRESHOLD_RANGE-1);
+					fprintf(stderr, "use option -h for detailed help\n");
 					return 1;
 				}
 				if (channel < 0 || channel >= TDC_N_CHANNELS) {
 					fprintf(stderr, "invalid channel number %d, must be in range [%d,%d]\n", channel, 0, TDC_N_CHANNELS);
+					fprintf(stderr, "use option -h for detailed help\n");
 					return 1;
 				}
 				thresholds[channel] = threshold;
 				break; 
 			case ':': 
 				printf("option needs a value\n"); 
+				fprintf(stderr, "use option -h for detailed help\n");
 				break; 
 			case '?': 
 				printf("unknown option: %c\n", optopt); 
+				fprintf(stderr, "use option -h for detailed help\n");
 				break; 
 		} 
 	} 
